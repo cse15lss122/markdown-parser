5cdf702222c1d823466da800ce4ccdd1b37e4cc9
jdai0 <j5dai@ucsd.edu>
2022-07-07 09:25:00 -0700

    newTest1

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,27 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.nio.file.Path;
+
 public class MarkdownParseTest {
 
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void newTest1() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> returnList=MarkdownParse.getLinks(content);
+        ArrayList<String> expected=new ArrayList<String>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+        assertEquals(returnList, expected);
     }
 
 }
