9ca2338b94b567a32862ce1f66d2b1fe092e5088
Wumboon <108438921+Wumboon@users.noreply.github.com>
2022-07-14 09:46:38 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -24,7 +24,7 @@ public class MarkdownParse {
 
 
     public static void main(String[] args) throws IOException {
-        System.out.println("hello other");
+        //System.out.println("hello other");
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
